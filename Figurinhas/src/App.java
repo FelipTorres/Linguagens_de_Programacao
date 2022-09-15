c65f18ca453b25961ab5d30d578db86cd513769c
Patrick Siqueira <psique.estuda@gmail.com>
2022-09-14 22:17:50 -0300

    Construction of the program welcome and auxiliary texts class

--- Figurinhas/src/App.java
+++ Figurinhas/src/App.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
+import br.com.figurinhas.TextosApp;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    static Scanner ler = new Scanner(System.in);
+    static TextosApp texto = new TextosApp();
+    public static void main(String[] args){
+        String opcao = null;
+
+        texto.boasVindas(coletarNomeUsuario());
+        texto.mostraMenuPrincipal();
+        opcao = ler.next();
+
+        switch (opcao) {
+            case "1":
+
+                break;
+        
+            default:
+                    texto.opcaoInvalida();
+                break;
+        }
+
+
+    }
+    private static String coletarNomeUsuario() 
+    {
+        texto.coletarNomeUsuario();
+        return ler.nextLine();
     }
 }
---
+++ Figurinhas/src/br/com/figurinhas/TextosApp.java
@@ -0,0 +1,27 @@
+package br.com.figurinhas;
+
+public class TextosApp {
+    public TextosApp() 
+    {
+    }
+
+    public void  mostraMenuPrincipal ()
+    {
+        System.out.print("O que você deseja fazer?\n  1 - Inserir figurinhas\n  2 - Ver figurinhas \n  3 - Ver idade média \n  4 - Sair \n   Opção Desejada -> ");
+    }
+
+    public void boasVindas (String nomeUsuário)
+    {
+        System.out.println("Olá "+ nomeUsuário +", seja Bem-vindo!");
+    }
+
+    public void coletarNomeUsuario() 
+    {
+        System.out.print("Olá, tudo bem?? \n Antes de começarmos insira seu nome para uma melhor experiência !! \n Insira seu nome ->");
+    }
+
+    public void opcaoInvalida() 
+    {
+        System.out.println("Opção inválida, jogador!");
+    }
+}
