0e201bfeb8fbf48ac622fdb60e86d0f48ebb7ec9
Patrick Siqueira <psique.estuda@gmail.com>
2022-10-25 21:41:43 -0300

    Segundo exercicio

--- Exercicio_Fixacao_ArrayList/src/br/com/fruta/Principal.java
+++ Exercicio_Fixacao_ArrayList/src/br/com/fruta/Principal.java
@@ -1,7 +1,19 @@
 package br.com.fruta;
 
+import java.util.ArrayList;
+
 public class Principal {
     public static void main(String[] args) {
-        
+        Fruta fruta0 = new Fruta("uva");
+        Fruta fruta1 = new Fruta("melancia");
+        Fruta fruta2 = new Fruta("banana");
+
+        Sacolao sacolao = new Sacolao("Sacolao", new ArrayList<Fruta>());
+
+        sacolao.AdicionarFruta(fruta0);
+        sacolao.AdicionarFruta(fruta1);
+        sacolao.AdicionarFruta(fruta2);
+
+        System.out.println(sacolao.getQuantidadeFruta());
     }
 }
--- Exercicio_Fixacao_ArrayList/src/br/com/fruta/Sacolao.java
+++ Exercicio_Fixacao_ArrayList/src/br/com/fruta/Sacolao.java
@@ -6,6 +6,11 @@ public class Sacolao {
     private String nome;
     private ArrayList<Fruta> listaFruta = new ArrayList<Fruta>();
 
+    public Sacolao(String nome, ArrayList<Fruta> listaFruta) {
+        this.nome = nome;
+        this.listaFruta = listaFruta;
+    }
+
     void AdicionarFruta(Fruta fruta) {
         listaFruta.add(fruta);
     }
