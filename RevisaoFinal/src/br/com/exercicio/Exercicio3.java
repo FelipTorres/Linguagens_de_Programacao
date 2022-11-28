eac2e7b46dc024166fc9fb3d55054cc8d9040b81
Patrick Siqueira <psique.estuda@gmail.com>
2022-11-28 11:05:57 -0300

    Calculadora

--- RevisaoFinal/src/br/com/exercicio/Exercicio3.java
+++ RevisaoFinal/src/br/com/exercicio/Exercicio3.java
@@ -1,9 +1,12 @@
 package br.com.exercicio;
+import java.awt.*;
+
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Exercicio3 {
 
@@ -14,16 +17,20 @@ public class Exercicio3 {
         JLabel nome =new JLabel("Nome ");
         JLabel email =new JLabel("E-mail");
         JButton butao = new JButton("OK");
-        JLabel labelCEP = new JLabel("CEP:");
-        labelCEP.setBounds(50,40,100,20);
+        
+                JTextField tfname = new JTextField();
+                JTextField tfEmail = new JTextField();
 
         JPanel painel = new JPanel();
 
-
-        painel.add(labelCEP);
         painel.add(nome);
+        painel.add(tfname);
         painel.add(email);
+        painel.add(tfEmail);
         painel.add(butao);
+
+        painel.setLayout(new GridLayout(3, 2, 5, 5));
+
 
         janela.getContentPane().add(painel);
         janela.setSize(300,200);
--- RevisaoFinal/src/br/com/exercicio/Exercicio4.java
+++ RevisaoFinal/src/br/com/exercicio/Exercicio4.java
@@ -1,38 +1,71 @@
 package br.com.exercicio;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+
 
 public class Exercicio4 {
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Calculator");
-        JPanel panel = new JPanel(null);
+        JFrame janela = new JFrame("Calculator");
+        JPanel painelTeclas = new JPanel(null);
+        JPanel painelAreaTexto = new JPanel(null);
+        Color corBackGround = new Color(128, 128, 128);
 
-        panel.add(new JButton("7"));
-        panel.add(new JButton("8"));
-        panel.add(new JButton("9"));
-        panel.add(new JButton("/"));
-        panel.add(new JButton("4"));
-        panel.add(new JButton("5"));
-        panel.add(new JButton("6"));
-        panel.add(new JButton("*"));
-        panel.add(new JButton("1"));
-        panel.add(new JButton("2"));
-        panel.add(new JButton("3"));
-        panel.add(new JButton("."));
-        panel.add(new JButton("0"));
-        panel.add(new JButton("-"));
+        JTextArea jtAreaTexto = new JTextArea();
 
-        panel.add(new JButton("="));
-        panel.add(new JButton("+"));
+        JButton jbSete = new JButton("7");
+        JButton jbOito = new JButton("8");
+        JButton jbNove = new JButton("9");
+        JButton jbDivisao = new JButton("/");
+        JButton jbQuatro = new JButton("4");
+        JButton jbCinco = new JButton("5");
+        JButton jbSeis = new JButton("6");
+        JButton jbMultiplicação = new JButton("*");
+        JButton jbUm = new JButton("1");
+        JButton jbDois = new JButton("2");
+        JButton jbTres = new JButton("3");
+        JButton jbPonto = new JButton(".");
+        JButton jbZero = new JButton("0");
+        JButton jbMenos = new JButton("-");
+
+        JButton jbIgual = new JButton("=");
+        JButton jbMais = new JButton("+");
+
+        painelTeclas.add(jbSete);
+        painelTeclas.add(jbOito);
+        painelTeclas.add(jbNove);
+        painelTeclas.add(jbDivisao);
+        painelTeclas.add(jbQuatro);
+        painelTeclas.add(jbCinco);
+        painelTeclas.add(jbSeis);
+        painelTeclas.add(jbMultiplicação);
+        painelTeclas.add(jbUm);
+        painelTeclas.add(jbDois);
+        painelTeclas.add(jbTres);
+        painelTeclas.add(jbPonto);
+        painelTeclas.add(jbZero);
+        painelTeclas.add(jbMenos);
+        painelTeclas.add(jbIgual);
+        painelTeclas.add(jbMais);
 
 
-        frame.setSize(300,300);
 
-        frame.add(panel);
-        frame.setVisible(true);
+        painelAreaTexto.add(jtAreaTexto);
+
+        painelTeclas.setLayout(new GridLayout(4, 4, 5, 5));
+        painelAreaTexto.setLayout(new GridLayout(1, 1, 5, 5));
+        janela.setLayout(new GridLayout(2, 1, 5, 5));
+
+
+        janela.setSize(300, 300);
+        painelAreaTexto.setBackground(corBackGround);
+        painelTeclas.setBackground(corBackGround);
+
+
+        janela.add(jtAreaTexto);
+        janela.add(painelTeclas);
+        janela.setVisible(true);
     }
 
 }
